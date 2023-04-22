591ac40ccc77552c8243118c2195fa52fdf06fbe
the-name-romel <racopra@sorsu.edu.ph>
2023-04-22 20:54:57 +0800

    init register modal

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return <div></div>;
+};
+
+export default RegisterModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 export const metadata = {
   title: "AirBnB",
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal isOpen title="Hello World" />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
