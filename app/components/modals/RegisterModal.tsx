aedd7c69148f5e551144f1c1e0e97cf45075a850
the-name-romel <racopra@sorsu.edu.ph>
2023-05-21 18:09:40 +0800

    Finish Authentication

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,6 +11,8 @@ import Heading from "../Heading";
 import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
+import { sign } from "crypto";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -80,13 +82,13 @@ const RegisterModal = () => {
           outline
           label="Continue with Google"
           icon={FcGoogle}
-          onClick={() => {}}
+          onClick={() => signIn("google")}
         />
         <Button
           outline
           label="Continue with Github"
           icon={AiFillGithub}
-          onClick={() => {}}
+          onClick={() => signIn("github")}
         />
       </div>
       <div className="text-neutral-500 text-center mt-4 font-light">
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -12,7 +12,6 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
-  console.log({ currentUser });
   return (
     <div className="fixed w-full bg-white shadow-sm z-10">
       <div className="py-4 border-b-[1px]">
